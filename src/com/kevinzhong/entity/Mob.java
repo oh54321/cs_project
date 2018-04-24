73bf5483ec79225b56b0cb621b0e154f15afaab4
oh54321 <38576852+oh54321@users.noreply.github.com>
2018-04-24 14:41:05 -0400

    Update Mob.java

--- src/com/kevinzhong/entity/Mob.java
+++ src/com/kevinzhong/entity/Mob.java
@@ -4,20 +4,37 @@ import com.kevinzhong.gfx.Animation;
 
 import java.awt.*;
 
-public abstract class Mob extends Entity {
+public class Mob extends Entity {
 
     private boolean leftAccel, rightAccel;
     private double moveSpeed;
     private Animation playerWalk;
-    private boolean facingRight;
-
+    private boolean facingRight, moonWalk;
+    private int health;
     public Mob(int w, int h) {
         super(w, h);
+        health =100;
         leftAccel = false;
         rightAccel = false;
         facingRight = true;
+        moonWalk = false;
     }
-
+    public Mob(int w, int h, int hlth) {
+        super(w, h);
+        health = hlth;
+        leftAccel = false;
+        rightAccel = false;
+        facingRight = true;
+        moonWalk = false;
+    }
+    public void setHealth(int h)
+    {
+        health = h;
+    }
+    public int getHealth()
+    {
+        return health;
+    }
     public void update() {
         move();
 
@@ -30,6 +47,7 @@ public abstract class Mob extends Entity {
             facingRight = true;
         else if(getxVel() < 0)
             facingRight = false;
+        moonWalk = rightAccel && leftAccel;
 
         if (!(leftAccel && rightAccel)) {
             super.setxVel(super.getxVel() * DECELERATION_RATE);
@@ -44,6 +62,13 @@ public abstract class Mob extends Entity {
     }
 
     public void render(Graphics2D g) {
+        if(moonWalk && !facingRight)
+            playerWalk.drawAnimation(g, (int) super.getBounds().getX(), (int) super.getBounds().getY(),
+                    (int) super.getBounds().getWidth(), (int) super.getBounds().getHeight(), false);
+        if (moonWalk && facingRight)
+            playerWalk.drawAnimation(g, (int) super.getBounds().getX(), (int) super.getBounds().getY(),
+                    (int) super.getBounds().getWidth(), (int) super.getBounds().getHeight(), true);
+            else
         playerWalk.drawAnimation(g, (int) super.getBounds().getX(), (int) super.getBounds().getY(),
                 (int) super.getBounds().getWidth(), (int) super.getBounds().getHeight(), facingRight);
     }
